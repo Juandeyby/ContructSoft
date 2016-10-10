cf12c62f3dec6cdc79fa217df2a391090335d466
Juandeyby <deyby57@gmail.com>
2016-10-09 23:56:16 -0500

    Ordenando

--- SoftSolution/src/Autentificador.java
+++ SoftSolution/src/Autentificador.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Autentificador {
@@ -45,33 +47,38 @@ public class Autentificador {
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usuario");
-		lblNewLabel.setBounds(50, 122, 115, 15);
+		lblNewLabel.setBounds(88, 105, 115, 15);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contraseña");
-		lblNewLabel_1.setBounds(50, 173, 115, 15);
+		lblNewLabel_1.setBounds(88, 157, 115, 15);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(50, 143, 114, 19);
+		textField.setBounds(78, 130, 300, 19);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(51, 202, 114, 19);
+		textField_1.setBounds(78, 184, 300, 19);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Universidad Nacional de San Agustín");
-		lblNewLabel_2.setBounds(78, 28, 300, 15);
+		lblNewLabel_2.setBounds(78, 38, 300, 15);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Inicie sesion para acceder");
-		lblNewLabel_3.setBounds(121, 55, 200, 15);
+		lblNewLabel_3.setBounds(121, 67, 200, 15);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		JButton btnNewButton = new JButton("Ingresar");
-		btnNewButton.setBounds(168, 239, 117, 25);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Codigo para poder enviar a la siguiente venta una vez inciacaido sesion
+			}
+		});
+		btnNewButton.setBounds(168, 215, 117, 25);
 		frame.getContentPane().add(btnNewButton);
 	}
 }
---
+++ SoftSolution/src/Olvido_contrasena.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class Olvido_contrasena {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Olvido_contrasena window = new Olvido_contrasena();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Olvido_contrasena() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
