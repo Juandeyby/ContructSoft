bc1716df6a05e4b74cf02331f3d45e1ea16098b7
Juandeyby <deyby57@gmail.com>
2016-10-10 00:18:39 -0500

    Imagen

--- SoftSolution/src/Autentificador.java
+++ SoftSolution/src/Autentificador.java
@@ -1,11 +1,15 @@
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.UIManager;
 
 
 public class Autentificador {
@@ -42,34 +46,35 @@ public class Autentificador {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(UIManager.getColor("Viewport.background"));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usuario");
-		lblNewLabel.setBounds(88, 105, 115, 15);
+		lblNewLabel.setBounds(112, 132, 115, 15);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contraseña");
-		lblNewLabel_1.setBounds(88, 157, 115, 15);
+		lblNewLabel_1.setBounds(112, 182, 115, 15);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(78, 130, 300, 19);
+		textField.setBounds(102, 148, 250, 19);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(78, 184, 300, 19);
+		textField_1.setBounds(102, 202, 250, 19);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Universidad Nacional de San Agustín");
-		lblNewLabel_2.setBounds(78, 38, 300, 15);
+		lblNewLabel_2.setBounds(138, 38, 300, 15);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Inicie sesion para acceder");
-		lblNewLabel_3.setBounds(121, 67, 200, 15);
+		lblNewLabel_3.setBounds(181, 67, 200, 15);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		JButton btnNewButton = new JButton("Ingresar");
@@ -78,7 +83,13 @@ public class Autentificador {
 				//Codigo para poder enviar a la siguiente venta una vez inciacaido sesion
 			}
 		});
-		btnNewButton.setBounds(168, 215, 117, 25);
+		btnNewButton.setBounds(168, 233, 117, 25);
 		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		Image img  = new ImageIcon(this.getClass().getResource("/logo_unsa.png")).getImage();
+		lblNewLabel_4.setIcon(new ImageIcon(img));
+		lblNewLabel_4.setBounds(44, 12, 82, 104);
+		frame.getContentPane().add(lblNewLabel_4);
 	}
 }
