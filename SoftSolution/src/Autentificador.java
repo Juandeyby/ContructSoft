4c7a075b2ef80a0eb6918e5aeb9b5ca18cc984b3
Juandeyby <deyby57@gmail.com>
2016-10-09 23:45:40 -0500

    Primera ventana

--- SoftSolution/src/Autentificador.java
+++ SoftSolution/src/Autentificador.java
@@ -1,11 +1,16 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 
 public class Autentificador {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -37,6 +42,36 @@ public class Autentificador {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Usuario");
+		lblNewLabel.setBounds(50, 122, 115, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Contraseña");
+		lblNewLabel_1.setBounds(50, 173, 115, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(50, 143, 114, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(51, 202, 114, 19);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Universidad Nacional de San Agustín");
+		lblNewLabel_2.setBounds(78, 28, 300, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Inicie sesion para acceder");
+		lblNewLabel_3.setBounds(121, 55, 200, 15);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("Ingresar");
+		btnNewButton.setBounds(168, 239, 117, 25);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
